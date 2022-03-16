3ef7420dd4fbf73e4fef29eb55c9bfefec79648c
BARBERET Remy <remy93130@gmail.com>
2022-03-16 13:14:29 +0100

    clean code

--- robot.py
+++ robot.py
@@ -1,126 +1,122 @@
+import random
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+import progressbar
 
 # [N, E, NE]
 weight = list(range(3))
 
-def N(x,y,m) :
-    if x == m-1 : return float("inf") 
+def N(x, y, m):
+    if x == m-1:
+        return float("inf")
     return weight[0]
 
 
-def E(x,y,n) :
-    if y == n-1 : return float("inf")
+def E(x, y, n):
+    if y == n-1:
+        return float("inf")
     return weight[1]
 
 
-def NE(x,y,m,n) :
-    if x == m-1 or y == n-1 : return float("inf")
+def NE(x, y, m, n):
+    if x == m - 1 or y == n - 1:
+        return float("inf")
     return weight[2]
 
 
-def calculerM(m,n) :
+def compute_M(m, n):
+    """O(m x n)"""
     M = [[float("inf") for c in range(n)] for l in range(m)]
-    # base de la récurrence
+
     M[0][0] = 0
     for x in range(1, m):
-    	M[x][0] = M[x-1][0] + N(x-1,0,m)
+        M[x][0] = M[x - 1][0] + N(x - 1, 0, m)
     for y in range(1,n):
-    	M[0][y] = M[0][y-1] + E(0,y-1,n)
-    # cas général : 1 ≤ x < m, 1 ≤ y < n
-    for x in range(1,m) : 
-        for y in range(1,n) :
+        M[0][y] = M[0][y - 1] + E(0, y - 1, n)
+
+    for x in range(1,m):
+        for y in range(1,n):
             M[x][y] = min(
-                N(x-1,y,m) + M[x-1][y], 
-                E(x,y-1,n) + M[x][y-1],
-                NE(x-1,y-1,m,n) + M[x-1][y-1] 
+                N(x - 1, y, m) + M[x - 1][y],
+                E(x, y - 1, n) + M[x][y - 1],
+                NE(x - 1, y - 1, m, n) + M[x - 1][y - 1]
             )
     return M
-    # complexité en Theta(m x n)
 
 
-def accm(x,y,M,m,n) :
-    if (x,y) == (0,0) : 
+def accm(x,y,M,m,n):
+    if (x, y) == (0, 0):
         return 0
-    if y == 0 :
-        return N(x-1,0,m) + accm(x-1,0,M,m,n) 
-    if x == 0 :
-        return accm(0,y-1,M,m,n) + E(0,y-1,n)
+    if y == 0:
+        return N(x - 1, 0, m) + accm(x - 1, 0, M, m, n)
+    if x == 0:
+        return accm(0, y - 1, M, m, n) + E(0, y - 1, n)
 
-    # cas général : 1 ≤ x < m, 1 ≤ y < n
-    cminN = N(x-1,y,m) + M[x-1][y] # coût min si dernier mvt N. 
-    cminE = E(x,y-1,n) + M[x][y-1] # coût min si dernier mvt E. 
-    cminNE = NE(x-1,y-1,m,n) + M[x-1][y-1] # coût min si dernier mvt NE.
+    cminN = N(x - 1, y, m) + M[x - 1][y]
+    cminE = E(x, y - 1, n) + M[x][y - 1]
+    cminNE = NE(x - 1, y - 1,m, n) + M[x - 1][y - 1]
 
-    if cminN <= min(cminE,cminNE) : # le dernier mvt a été N
-        return accm(x-1,y,M,m,n) + N(x-1,y,m)
-    elif cminE <= cminNE : # le dernier mvt a été E
-        return accm(x,y-1,M,m,n) + E(x,y-1,n)
-    else : # le dernier mvt a été NE
-        return accm(x-1,y-1,M,m,n) + NE(x-1,y-1,m,n)
+    if cminN <= min(cminE, cminNE):
+        return accm(x - 1, y, M, m, n) + N(x - 1, y, m)
+    elif cminE <= cminNE:
+        return accm(x, y - 1, M, m, n) + E(x, y - 1, n)
+    else:
+        return accm(x - 1, y - 1, M, m, n) + NE(x - 1, y - 1, m, n)
 
 
 def glouton_robot(x, y, M, m, n):
-    if((x,y) == (0,0)):
+    if (x, y) == (0, 0):
         return 0
-    if y == 0 :
-        return N(x-1,0,m) + glouton_robot(x-1,0,M,m,n) # le ccm de (0,0) à (x-1,0) est affiché
-    if x == 0 :
-        return glouton_robot(0,y-1,M,m,n) + E(0,y-1,n)
-    # cas général : 1 ≤ x < m, 1 ≤ y < n
-    cminN = N(x-1,y,m) 
-    cminE = E(x,y-1,n)
-    cminNE = NE(x-1,y-1,m,n)
-    if cminN <= min(cminE,cminNE) : # le dernier mvt a été N
-        return glouton_robot(x-1,y,M,m,n) + cminN
-    elif cminE <= cminNE : # le dernier mvt a été E
-        return glouton_robot(x,y-1,M,m,n) + cminE # le ccm de (0,0) à (x,y-1) a été affiché.
-    else : # le dernier mvt a été NE
-        return glouton_robot(x-1,y-1,M,m,n) + cminNE# le ccm de (0,0) à (x-1,y-1) a été affiché.
+    if y == 0:
+        return N(x - 1, 0, m) + glouton_robot(x - 1, 0, M, m, n)
+    if x == 0:
+        return glouton_robot(0, y - 1, M, m, n) + E(0, y - 1, n)
+
+    cminN = N(x - 1, y, m)
+    cminE = E(x, y - 1, n)
+    cminNE = NE(x - 1, y - 1, m, n)
+    if cminN <= min(cminE, cminNE):
+        return glouton_robot(x - 1, y, M, m, n) + cminN
+    if cminE <= cminNE:
+        return glouton_robot(x, y - 1, M, m, n) + cminE
+    return glouton_robot(x - 1, y - 1, M, m, n) + cminNE
 
 
-def afficher(M) :
-    m,n = len(M),len(M[0])
-    for x in range(m-1,-1,-1) :
+def afficher(M):
+    m, n = len(M), len(M[0])
+    for x in range(m-1, -1, -1):
         line = ""
-        for y in range(n) : 
-            line += str(M[x][y]) + ",\t"
+        for y in range(n):
+            line += f"{M[x][y]},\t"
         print(line[:-2])
 
 
-def main() :
-    N = 1000
-    distance_relative = list()
+def benchmark_robot(runs):
+    relative_distance = list()
     values_optimal = list()
     values_greedy = list()
 
-    for i in range (N):
-        #Une matrice variante de taille (2,2) à (10,10)
-        m = random.randrange(2, 10)
-        n = random.randrange(2, 10)
+    for _ in progressbar.progressbar(range(runs)):
+        m, n = random.randint(10, 100), random.randint(10, 100)
         for j in range(2):
-            weight[j] = random.randrange(1, 3) #On change les poids des directions entre 1 et 3
-        
-        M = calculerM(m, n)
-        optimal = accm(m-1,n-1,M,m,n)
-        glouton = glouton_robot(m-1,n-1,M,m,n)
-        values_optimal.append(optimal)
-        values_greedy.append(glouton)
-        distance_relative.append((glouton - optimal) / optimal)
+            weight[j] = random.randint(1, 4)
+        M = compute_M(m,  n)
+        value_optimal = accm(m - 1, n - 1, M, m, n)
+        value_greedy = glouton_robot(m - 1, n - 1, M, m, n)
 
-    num_bins = N // 2
-    mean_greedy = np.mean(glouton)
-    std_greedy = np.std(glouton)
+        values_optimal.append(value_optimal)
+        values_greedy.append(value_greedy)
+        relative_distance.append((value_greedy - value_optimal) / value_optimal)
+    mean_greedy = np.mean(values_greedy)
+    mean_optimal = np.mean(value_optimal)
+    std_greedy = np.std(values_greedy)
+    std_optimal = np.std(values_optimal)
 
-    mean_optimal = np.mean(optimal)
-    std_optimal = np.std(optimal)
-
-    print(f"Greedy :\n\tmean:{mean_greedy}\n\tstd:{std_greedy}")
-    print(f"Optimal :\n\tmean:{mean_optimal}\n\tstd:{std_optimal}")
-    plt.hist(distance_relative, num_bins)
+    print(f"Greedy:\n\tmean:{mean_greedy}\n\tstd:{std_greedy}")
+    print(f"Optimal:\n\tmean:{mean_optimal}\n\tstd:{std_optimal}")
+    plt.hist(relative_distance, bins='auto')
     plt.show()
 
 
 if __name__ == '__main__':
-	main()
+    benchmark_robot(10000)
